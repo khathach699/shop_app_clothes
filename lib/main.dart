62db3b81cd2334ec3131bf31e0b1aaa4d2c6467e
KhaThach <khathach699@gmail.com>
2025-01-02 15:01:20 +0700

    Essential Utilities, Helper Functions, Constants and much more

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:shop_app_clothes/app.dart';
-import 'package:shop_app_clothes/utils/constants/colors.dart';
 
 void main() {
   runApp(const App());
